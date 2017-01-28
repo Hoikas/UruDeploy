961e25c60dcc41a5afd15819033ed7743577410c
Adam Johnson <AdamJohnso@gmail.com>
2017-01-27 22:37:41 -0500

    Convert os.path usage to pathlib

--- build_cwe.py
+++ build_cwe.py
@@ -13,7 +13,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with UruDeploy.  If not, see <http://www.gnu.org/licenses/>.
 
-import os.path
+import pathlib
 import shutil
 import subprocess
 
@@ -22,10 +22,14 @@ from builder import BuildProcess
 class CWEBuilder(BuildProcess):
     def __init__(self, config):
         self.build_id = config.get("client", "build_id")
-        self.build_dir = config.get("cwe", "build_dir")
+        self.build_dir = pathlib.Path(config.get("cwe", "build_dir"))
         self.internal_client = config.getboolean("cwe", "build_internal")
         self.external_client = config.getboolean("cwe", "build_external")
-        self.staging_dir = config.get("client", "staging_dir")
+        self.staging_dir = pathlib.Path(config.get("client", "staging_dir"))
+        try:
+            self.staging_dir.mkdir(parents=True)
+        except FileExistsError:
+            pass
 
     def run(self):
         # Unfortunately, this needs to be serialized...
@@ -40,23 +44,33 @@ class CWEBuilder(BuildProcess):
 
     def _build_client(self, isExternal):
         # CMake options...
-        cmake = "cmake \"{}\" -DPLASMA_BUILD_LAUNCHER=ON -DPLASMA_BUILD_TOOLS=ON -DPRODUCT_BUILD_ID={}".format(self.build_dir, self.build_id)
+        client_root = self.build_dir.resolve()
+        cmake = "cmake \"{}\" -DPLASMA_BUILD_LAUNCHER=ON -DPLASMA_BUILD_TOOLS=ON -DPRODUCT_BUILD_ID={}".format(client_root, self.build_id)
         if isExternal:
-            cmake = "{} -DPLASMA_EXTERNAL_RELEASE".format(cmake)
-        subprocess.call(cmake, shell=True)
+            cmake = "{} -DPLASMA_EXTERNAL_RELEASE=ON".format(cmake)
+        else:
+            cmake = "{} -DPLASMA_EXTERNAL_RELEASE=OFF".format(cmake)
+        subprocess.call(cmake, shell=True, cwd=str(self.build_dir))
 
         # Okay, donedone that. Now, we must run MSBuild
-        sln = os.path.join(self.build_dir, "Plasma.sln")
+        sln = self.build_dir.joinpath("Plasma.sln")
         msbuild = "MSBuild.exe /maxcpucount /nologo /property:Configuration=RelWithDebInfo /target:Rebuild \"{}\"".format(sln)
         subprocess.call(msbuild, shell=True)
 
         # Build folder renaming, cause we don't want our PDBs to be blown away
-        bin = os.path.join(self.build_dir, "bin")
-        suffix = "_external" if isExternal else "_internal"
+        bin_dir = self.build_dir.joinpath("bin")
+        suffix = "external" if isExternal else "internal"
         suffix = "{}{}".format(suffix, self.build_id)
-        src = os.path.join(bin, "RelWithDebInfo")
-        dest = os.path.join(bin, "RelWithDebInfo_{}".format(suffix))
+        src = bin_dir.joinpath("RelWithDebInfo")
+        dest = bin_dir.joinpath("RelWithDebInfo_{}".format(suffix))
 
-        if os.path.exists(dest):
-            shutil.rmtree(dest, True)
-        shutil.move(src, dest)
+        if dest.exists():
+            shutil.rmtree(str(dest), True)
+        shutil.move(str(src), str(dest))
+
+        # Copy client binaries to the staging directory
+        for i in {"plClient.exe", "plCrashHandler.exe", "plUruLauncher.exe",
+                  "UruCrashHandler.exe", "UruExplorer.exe", "UruLauncher.exe"}:
+            exe_src = dest.joinpath(i)
+            if exe_src.is_file():
+                shutil.copy(str(exe_src), str(self.staging_dir))
